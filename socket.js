c537a04edd86340a49b0a35c75c225b211cc4f5d
Oleksandr Martyshchenko <ketler13@yandex.ru>
2018-12-07 07:59:33 +0200

    Fix operators length calculation

--- socket.js
+++ socket.js
@@ -34,32 +34,28 @@ function socket(server) {
 
   function onOperatorConnected(operator) {
     const operatorId = operator.id;
-    console.log('operator connected', operatorId);
     addOperatorToActive(operatorId);
     notifyCustomersAboutOperatorsLength();
     operator.on(ACCEPT_CALL, onOperatorAcceptCall);
-    operator.on(FINISH_CALL, () => onOperatorDisconnected(operatorId));
+    operator.on(FINISH_CALL, () => onOperatorFinishedCall(operatorId));
     operator.on(DISCONNECT, () => onOperatorDisconnected(operatorId));
   }
 
   function onCustomerConnected(customer) {
-    console.log('customer connected', customer.id);
     const customerId = customer.id;
     customer.on(REQUEST_CALL, onCustomerRequestCall);
-    customer.on(FINISH_CALL, () => onCustomerDisconnected(customerId));
+    customer.on(FINISH_CALL, () => onCustomerFinishedCall(customerId));
     customer.on(DISCONNECT, () => onCustomerDisconnected(customerId));
     customer.on(OPERATORS_LENGTH, () => notifyCustomerAboutOperatorsLength(customerId));
   }
 
   function onCustomerRequestCall() {
-    console.log('incoming call from customer', this.id);
     operators.to(ACTIVE_OPERATORS).emit(INCOMING_CALL, { customerId: this.id });
   }
 
   function onOperatorAcceptCall(data) {
     const operatorId = this.id;
     const { customerId } = data.query;
-    console.log('operator accepted call', operatorId, '--->', customerId);
     removeOperatorFromActive(operatorId);
     notifyCustomersAboutOperatorsLength();
     checkAndCreateRoom(operatorId)
@@ -76,19 +72,26 @@ function socket(server) {
       .emit(CALL_ACCEPTED, operatorId);
   }
 
+  function onOperatorFinishedCall(operatorId) {
+    addOperatorToActive(operatorId);
+    onOperatorDisconnected(operatorId);
+  }
+
   function onOperatorDisconnected(operatorId) {
     const customerId = getCustomerIdByOperatorId(operatorId);
-    console.log('operator disconnected', operatorId, customerId);
+    notifyCustomersAboutOperatorsLength();
     if (customerId) {
       notifyCustomerAboutOperatorDisconnection(customerId);
       unbindCustomerFromOperator(customerId, operatorId);
-      notifyCustomersAboutOperatorsLength();
     }
+  }
+
+  function onCustomerFinishedCall(customerId) {
+    onCustomerDisconnected(customerId);
   }
 
   function onCustomerDisconnected(customerId) {
     const operatorId = getOperatorIdByCustomerId(customerId);
-    console.log('customer disconnected', customerId, operatorId);
     if (operatorId) {
       notifyOperatorAboutCustomerDisconnection(operatorId);
       unbindCustomerFromOperator(customerId, operatorId);
@@ -132,29 +135,57 @@ function socket(server) {
   }
 
   function notifyCustomersAboutOperatorsLength() {
-    console.log('notifyCustomersAboutOperatorsLength');
-    customers.emit(OPERATORS_LENGTH, getOperatorsLengthData());
+    // customers.emit(OPERATORS_LENGTH, getOperatorsLengthData());
+    return getOperatorsLengthData()
+      .then(data => customers.emit(OPERATORS_LENGTH, data));
   }
 
   function notifyCustomerAboutOperatorsLength(customerId) {
-    console.log('notifyCustomerAboutOperatorsLength');
-    customers.to(customerId).emit(OPERATORS_LENGTH, getOperatorsLengthData());
+    // customers.to(customerId).emit(OPERATORS_LENGTH, getOperatorsLengthData());
+    return getOperatorsLengthData()
+      .then(data => customers.to(customerId).emit(OPERATORS_LENGTH, data));
   }
 
   function getOperatorsLengthData() {
-    return {
-      all: getAllOperatorsLength(),
-      active: getActiveOperatorsLength(),
-    };
+    const promises = [getAllOperatorsLength(), getActiveOperatorsLength()];
+    return Promise.all(promises)
+      .then(([all, active]) => ({ all, active }));
+    // return {
+    //   all: getAllOperatorsLength(),
+    //   active: getActiveOperatorsLength(),
+    // };
   }
 
   function getActiveOperatorsLength() {
-    const activeOperators = operators.in(ACTIVE_OPERATORS).connected;
-    return Object.keys(activeOperators).length;
+    // operators.in(ACTIVE_OPERATORS).clients((err, clients) => {
+    //   console.log('CLIENTS', clients.length);
+    // });
+    // const activeOperators = operators.in(ACTIVE_OPERATORS).connected;
+    // return Object.keys(activeOperators).length;
+    return new Promise((resolve, reject) => {
+      operators.in(ACTIVE_OPERATORS).clients((err, clients) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log('active operators', clients.length);
+          resolve(clients.length);
+        }
+      });
+    });
   }
 
   function getAllOperatorsLength() {
-    return Object.keys(operators.connected).length;
+    // return Object.keys(operators.connected).length;
+    return new Promise((resolve, reject) => {
+      operators.clients((err, clients) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log('all operators', clients.length);
+          resolve(clients.length);
+        }
+      });
+    });
   }
 }
 
