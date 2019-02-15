46306983a35ead8b0a33692a617bba67fbf09f06
Oleksandr Martyshchenko <ketler13@yandex.ru>
2019-02-15 15:27:33 +0200

    Remove customer/operator id from feedback

--- src/models/schemas/customerFeedback.js
+++ src/models/schemas/customerFeedback.js
@@ -1,10 +1,5 @@
-const { CUSTOMER_ID_MISSING } = require('@/constants/feedbackErrors');
 const feedback = require('./feedback');
 
 module.exports = {
   ...feedback,
-  customerId: {
-    required: CUSTOMER_ID_MISSING,
-    type: String,
-  },
 };
--- src/models/schemas/operatorFeedback.js
+++ src/models/schemas/operatorFeedback.js
@@ -1,12 +1,7 @@
-const { OPERATOR_ID_MISSING } = require('@/constants/feedbackErrors');
 const feedback = require('./feedback');
 
 module.exports = {
   ...feedback,
-  operatorId: {
-    required: OPERATOR_ID_MISSING,
-    type: String,
-  },
   callType: {
     type: String,
   },
