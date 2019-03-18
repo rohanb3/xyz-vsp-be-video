f18aae8ada42b0f8aeb26974fe079b83e582cebf
Oleksandr Martyshchenko <ketler13@yandex.ru>
2019-03-18 16:07:29 +0200

    Add some logs

--- src/services/socketAdapter.js
+++ src/services/socketAdapter.js
@@ -6,6 +6,9 @@ const { REDIS_HOST, REDIS_PORT, REDIS_OPTIONS } = require('@/constants/redis');
 const pubClient = redis.createClient(REDIS_PORT, REDIS_HOST, REDIS_OPTIONS);
 const subClient = redis.createClient(REDIS_PORT, REDIS_HOST, REDIS_OPTIONS);
 
+pubClient.on('error', err => console.error('pubClient: ', err));
+subClient.on('error', err => console.error('subClient: ', err));
+
 const createAdapter = () => redisAdapter({ pubClient, subClient });
 
 exports.createAdapter = createAdapter;
