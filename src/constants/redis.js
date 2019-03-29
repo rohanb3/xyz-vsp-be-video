52ba4037cda952351babd862dfa431beafd90e77
Oleksandr Martyshchenko <ketler13@yandex.ru>
2019-03-29 12:34:04 +0200

    Add node_env production to process.yml

--- src/constants/redis.js
+++ src/constants/redis.js
@@ -13,6 +13,8 @@ const options = authRequired
   }
   : {};
 
+console.log(host, port, authRequired, password);
+
 exports.REDIS_HOST = host;
 exports.REDIS_PORT = port;
 exports.REDIS_PASSWORD = password;
