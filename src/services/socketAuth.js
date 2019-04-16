93d15dc2822c1254eb04d981a00529a266ae3213
Oleksandr Martyshchenko <ketler13@yandex.ru>
2019-04-16 12:07:40 +0300

    11098143 - Add tests for socket auth

--- src/services/socketAuth.js
+++ src/services/socketAuth.js
@@ -1,19 +1,20 @@
-const { getToken } = require('@/services/twilio');
+const twilio = require('@/services/twilio');
 const { connectionsHeap } = require('@/services/connectionsHeap');
 
 const NO_IDENTITY = 'identity.not.provided';
+const NO_DEVICE_ID = 'device.id.not.provided';
 const ALREADY_LOGGED_IN = 'already.logged.in';
 
 function authenticateOperator(socket, data, callback) {
   const { identity } = data;
   if (!identity) {
-    return callback(new Error(NO_IDENTITY));
+    return Promise.reject(new Error(NO_IDENTITY)).catch(callback);
   }
   return connectionsHeap.isExist(identity).then((isExist) => {
     if (isExist) {
       return callback(new Error(ALREADY_LOGGED_IN));
     }
-    const token = getToken(identity);
+    const token = twilio.getToken(identity);
     socket.identity = identity;
     return callback(null, token);
   });
@@ -22,12 +23,15 @@ function authenticateOperator(socket, data, callback) {
 function authenticateCustomer(socket, data, callback) {
   const { identity, deviceId } = data;
   if (!identity) {
-    return callback(new Error(NO_IDENTITY));
+    return Promise.reject(new Error(NO_IDENTITY)).catch(callback);
   }
-  const token = getToken(identity);
+  if (!deviceId) {
+    return Promise.reject(new Error(NO_DEVICE_ID)).catch(callback);
+  }
+  const token = twilio.getToken(identity);
   socket.identity = identity;
   socket.deviceId = deviceId;
-  return callback(null, token);
+  return Promise.resolve().then(() => callback(null, token));
 }
 
 exports.authenticateOperator = authenticateOperator;
