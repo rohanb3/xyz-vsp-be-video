f14b87471fbc35a3555511db9d24fa5c0dbb9122
Igor Retiznyk <igor.retiznyk@ardas.biz>
2019-09-03 18:08:53 +0300

    fix head

--- src/rnd_calls.js
+++ src/rnd_calls.js
@@ -68,7 +68,7 @@ function tokenGenerator(request, response) {
   // token.addGrant(voiceGrant);
   token.identity = identity;
   console.log('Token:' + token.toJwt());
-  response.writeHead('Content-Type', 'text/plain');
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
   return response.send(token.toJwt());
 }
 
