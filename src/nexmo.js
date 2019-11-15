089e93b86f27df6ce674f471d7b0f6b8bb86fd16
artem.lupyr <artem.lupyr@ardas.biz>
2019-11-15 16:21:52 +0200

    ncco delete from nexmo

--- src/nexmo.js
+++ src/nexmo.js
@@ -1,10 +1,11 @@
 const Nexmo = require('nexmo');
 const logger = require('./services/logger')(module);
-const { 
-  NEXMO_API_KEY, 
-  NEXMO_API_SECRET, 
-  NEXMO_APPLICATION_ID, 
-  NEXMO_PATH_TO_PRIVATE_KEY, } = require('./constants/nexmo');
+const {
+  NEXMO_API_KEY,
+  NEXMO_API_SECRET,
+  NEXMO_APPLICATION_ID,
+  NEXMO_PATH_TO_PRIVATE_KEY,
+} = require('./constants/nexmo');
 
 const API_KEY = NEXMO_API_KEY;
 const API_SECRET = NEXMO_API_SECRET;
@@ -18,70 +19,67 @@ const nexmo = new Nexmo({
   privateKey: PATH_TO_PRIVATE_KEY,
 });
 
-const ncco = [
-    {
-      action: 'talk',
-      voiceName: 'Joey',
-      text:
-        'This is a text-to-speech test message.',
-    },
-  ];
+// const ncco = [
+//   {
+//     action: 'talk',
+//     voiceName: 'Joey',
+//     text: 'This is a text-to-speech test message.',
+//   },
+// ];
 
-  const createCall = (request) => {
-    const { to, from } = request.params;
-    return new Promise((resolve) => {
-      nexmo.calls.create(
-        {
-          to: [{ type: 'phone', number: to }],
-          from: { type: 'phone', number: from},
-          ncco,
-        },
-        (err, result) => {
-          logger.debug(err || result);
-          resolve(err || result);
-        },
-      );
-    })
-  }
+const createCall = request => {
+  const { to, from } = request.params;
+  return new Promise(resolve => {
+    nexmo.calls.create(
+      {
+        to: [{ type: 'phone', number: to }],
+        from: { type: 'phone', number: from },
+      },
+      (err, result) => {
+        logger.debug(err || result);
+        resolve(err || result);
+      }
+    );
+  });
+};
 
-  const endCall = (request) => {
-    const { callid } = request.params;
-    return new Promise((resolve) => {
-      nexmo.calls.update(
-        callid,
-        {
-          action: 'hangup'
-        },
-        (err, result) => {
-          logger.debug(err || result);
-          resolve(err || result);
-        },
-      );
-    })
-  }
+const endCall = request => {
+  const { callid } = request.params;
+  return new Promise(resolve => {
+    nexmo.calls.update(
+      callid,
+      {
+        action: 'hangup',
+      },
+      (err, result) => {
+        logger.debug(err || result);
+        resolve(err || result);
+      }
+    );
+  });
+};
 
-  function getEvent(request, response) {
-    logger.debug('NEXMO EVENT :', request.params, request.body, request.query);
-    return response.status(200).send();
-  }
-  
-  function getAnswer(request, response) {
-    logger.debug('NEXMO ANSWER :', request.query);
-    return response.status(200).send();
-  }
+function getEvent(request, response) {
+  logger.debug('NEXMO EVENT :', request.params, request.body, request.query);
+  return response.status(200).send();
+}
 
-  async function finishCall(request, response) {
-    let result = await endCall(request);
-    return response.status(200).send(result);
-  }
+function getAnswer(request, response) {
+  logger.debug('NEXMO ANSWER :', request.query);
+  return response.status(200).send();
+}
 
-  async function startCall(request, response) {
-    let result = await createCall(request);
-    return response.status(200).send(result);
-  }
+async function finishCall(request, response) {
+  let result = await endCall(request);
+  return response.status(200).send(result);
+}
 
+async function startCall(request, response) {
+  let result = await createCall(request);
+  return response.status(200).send(result);
+}
 
-  exports.getEvent = getEvent;
-  exports.getAnswer = getAnswer;
-  exports.startCall = startCall;
-  exports.endCall = finishCall;
+exports.getEvent = getEvent;
+exports.getAnswer = getAnswer;
+exports.startCall = startCall;
+exports.endCall = finishCall;
