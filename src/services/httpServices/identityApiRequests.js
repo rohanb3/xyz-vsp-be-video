14fe7381d2e3b65202f9c7a9e7ae54153d2fb5c2
Igor Retiznyk <igor.retiznyk@ardas.biz>
2019-11-26 16:51:27 +0200

    support socket auth for customer

--- src/services/httpServices/identityApiRequests.js
+++ src/services/httpServices/identityApiRequests.js
@@ -1,11 +1,22 @@
 const axios = require('axios');
-
 const { IDENTITY_API_URL, STATIC_TOKEN } = require('@/constants/identityApi');
 
-async function getCompanyIdByUserId(identity) {
-  return axios
-    .get(`${IDENTITY_API_URL}/users/${STATIC_TOKEN}/trusted/${identity}`)
+const api = axios.create({
+  baseURL: IDENTITY_API_URL,
+});
+
+function getCompanyIdByUserId(identity) {
+  return api
+    .get(`users/${STATIC_TOKEN}/trusted/${identity}`)
     .then(response => response.data.companyId);
 }
 
+function checkTokenValidity(token) {
+  return api
+    .get('users/profile', { headers: { Authorization: `Bearer ${token}` } })
+    .then(() => true)
+    .catch(() => false);
+}
+
 exports.getCompanyIdByUserId = getCompanyIdByUserId;
+exports.checkTokenValidity = checkTokenValidity;
--- src/services/socketAuth.js
+++ src/services/socketAuth.js
@@ -6,6 +6,7 @@ const { connectionsHeap } = require('@/services/connectionsHeap');
 
 const NO_IDENTITY = 'identity.not.provided';
 const NO_DEVICE_ID = 'device.id.not.provided';
+const TOKEN_INVALID = 'token.invalid';
 
 async function authenticateOperator(
   disconnectCallBack,
@@ -37,24 +38,30 @@ async function authenticateOperator(
 }
 
 async function authenticateCustomer(socket, data, callback) {
-  const { identity, deviceId } = data;
+  const { identity, deviceId, token } = data;
 
   if (!identity) {
-    return Promise.reject(new Error(NO_IDENTITY)).catch(callback);
+    return callback(new Error(NO_IDENTITY));
   }
   if (!deviceId) {
-    return Promise.reject(new Error(NO_DEVICE_ID)).catch(callback);
+    return callback(new Error(NO_DEVICE_ID));
+  }
+
+  const tokenValid = !token || (await identityApi.checkTokenValidity(token));
+
+  if (!tokenValid) {
+    return callback(new Error(TOKEN_INVALID));
   }
 
   const companyId = await identityApi.getCompanyIdByUserId(identity);
   const tenantId = await publicApi.getTenantIdByCompanyId(companyId);
 
-  const token = twilio.getToken(identity);
+  const twilioToken = twilio.getToken(identity);
   socket.identity = identity;
   socket.deviceId = deviceId;
   socket.tenantId = tenantId;
 
-  callback(null, token);
+  callback(null, twilioToken);
 }
 
 exports.authenticateOperator = authenticateOperator;
