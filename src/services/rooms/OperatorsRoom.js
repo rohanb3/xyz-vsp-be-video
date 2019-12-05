0d2c730ab686ca564efa4bd2d916596a29861ac2
Igor Retiznyk <igor.retiznyk@ardas.biz>
2019-12-05 11:41:07 +0200

    update auth connection timeout

--- src/services/rooms/OperatorsRoom.js
+++ src/services/rooms/OperatorsRoom.js
@@ -57,7 +57,7 @@ class OperatorsRoom {
         this.disconnectOldSocket.bind(this)
       ),
       postAuthenticate: this.onOperatorAuthenticated.bind(this),
-      timeout: 10000,
+      timeout: 15000,
     });
 
     calls.subscribeToCallFinishing(this.onCallFinished.bind(this));
