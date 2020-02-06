e23815d0383724fe6e280ec7b3cdb6e855c34dba
Andrey Matushenko <andrii.matiushenko@ardas.dp.ua>
2020-02-06 11:23:53 +0200

    Fix tests for secure routes

---
+++ src/routes/__mocks__/utils.js
@@ -0,0 +1,11 @@
+const authenticateRequest = () => (req, res, next) => next();
+const setupSwagger = () => {};
+const validateRequest = () => (req, res, next) => next();
+const protectWithPermission = () => (req, res, next) => next();
+const setupValidateRequestMiddleware = () => (req, res, next) => next();
+
+exports.setupSwagger = setupSwagger;
+exports.validateRequest = validateRequest;
+exports.authenticateRequest = authenticateRequest;
+exports.protectWithPermission = protectWithPermission;
+exports.setupValidateRequestMiddleware = setupValidateRequestMiddleware;
